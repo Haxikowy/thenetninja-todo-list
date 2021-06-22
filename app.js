2fd5b8c09dfcbe0b56f6e26350b3440c4108b6ff
Szymon Dolnik <szymon.dolnik@gmail.com>
2021-06-22 21:49:27 +0200

    Add todolist - now you can add some of them, not only remove.

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const HTMLTemplate = `<li class="list-group-item d-flex justify-content-between align-items-center">
-                        <span>${'spanContent'}</span>
-                        <i class="far fa-trash-alt delete"></i>
-                      </li>`
 const todos = document.querySelector('.todos');
+const addTodoForm = document.querySelector('.add');
 
-todos.addEventListener('click', (e) => {
+
+
+todos.addEventListener('click', e => {
   if (e.target.classList[2] === "delete") {
     e.target.parentElement.remove();
   }
+});
+
+addTodoForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const HTMLTemplate = `<li class="list-group-item d-flex justify-content-between align-items-center">
+                        <span>${e.target[0].value.trim()}</span>
+                        <i class="far fa-trash-alt delete"></i>
+                       </li>`
+
+  todos.innerHTML += HTMLTemplate;
+
+  e.target[0].value = "";
 });
