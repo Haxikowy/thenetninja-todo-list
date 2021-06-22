31420077b0a5d28b6ded31f971e8c10fe5d0a747
Szymon Dolnik <szymon.dolnik@gmail.com>
2021-06-22 23:16:46 +0200

    Added search&filter function, also improved readability of the code

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 const todos = document.querySelector('.todos');
-const addTodoForm = document.querySelector('.add');
+const addForm = document.querySelector('.add');
+const searchForm = document.querySelector('.search')
 
 
+// Event listener needed to add some todo's
+addForm.addEventListener('submit', e => {
+  e.preventDefault();
 
+  const HTMLTemplate = `<li class="list-group-item d-flex justify-content-between align-items-center">
+                        <span>${addForm.add.value.trim()}</span>
+                        <i class="far fa-trash-alt delete"></i>
+                       </li>`
+  todos.innerHTML += HTMLTemplate;
+  addForm.reset();
+});
+
+// Event Listener needed to remove todo's
 todos.addEventListener('click', e => {
-  if (e.target.classList[2] === "delete") {
+  if (e.target.classList.contains('delete')) {
     e.target.parentElement.remove();
   }
 });
 
-addTodoForm.addEventListener('submit', e => {
-  e.preventDefault();
+// Search & filter todos
 
-  const HTMLTemplate = `<li class="list-group-item d-flex justify-content-between align-items-center">
-                        <span>${e.target[0].value.trim()}</span>
-                        <i class="far fa-trash-alt delete"></i>
-                       </li>`
+const filterTodos = text => {
+  Array.from(todos.children)
+    .filter(todo => !todo.textContent.toUpperCase().includes(text))
+    .forEach(todo => todo.classList.add('filtered'));
 
-  todos.innerHTML += HTMLTemplate;
+  Array.from(todos.children)
+    .filter(todo => todo.textContent.toUpperCase().includes(text))
+    .forEach(todo => todo.classList.remove('filtered'));
+}
 
-  e.target[0].value = "";
+searchForm.addEventListener('keyup', e => {
+  let text = e.target.value.trim().toUpperCase();
+
+  filterTodos(text);
 });
+
+searchForm.addEventListener('submit', e => e.preventDefault());
